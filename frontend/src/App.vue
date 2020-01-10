e531bb541acbf623734e247f97ce936edc9b2695
TheImaginaryOne <johnathanleung@hotmail.co.nz>
2020-01-10 15:25:45 +1300

    Modify styles of frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
-    <SearchForm @update:query="updateQuery"/>
-    <WordEntries v-for="word in words" :key="word.id" :word-entries="word"/>
+    <div class="header-container">
+      <div class="full-width header">
+        <div class="title">粵語詞典 Cantonese Dictionary</div>
+        <SearchForm @update:query="updateQuery"/>
+      </div>
+    </div>
+    <div class="full-width">
+      <WordEntries v-for="word in words" :key="word.id" :word-entries="word"/>
+    </div>
   </div>
 </template>
 
@@ -43,12 +50,32 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Fira+Sans:400,700&display=swap');
-#app {
+@import url('https://fonts.googleapis.com/css?family=Fira+Sans:400,500,700&display=swap');
+.full-width {
   font-family: 'Fira Sans', sans-serif;
   width: 100%;
   max-width: 1200px;
   margin: 0 auto;
   line-height: 1.5;
+}
+body {
+  margin: 0;
+}
+.header-container {
+  background-color: #6f8996;
+}
+.header {
+  display: flex;
+  padding-top: .75rem;
+  padding-bottom: 0.75rem;
+}
+.title {
+  font-size: 1.5rem;
+  font-weight: 500;
+  padding-right: 2rem;
+  color: #fff;
+}
+.nav-item {
+  padding-right: 0.5rem;
 }
 </style>
--- frontend/src/components/SearchForm.vue
+++ frontend/src/components/SearchForm.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="search-form">
-    <input v-model="query"/>
+    <div class="nav-item">
+      <select class="nav-select">
+        <option value="jyutping">Jyutping (Cantonese)</option>
+        <option value="pinyin">Pinyin (Mandarin)</option>
+        <option value="characters">Characters</option>
+        <option value="english">English</option>
+      </select>
+    </div>
+    <div class="nav-item">
+      <input class="nav-input" v-model="query"/>
+    </div>
   </div>
 </template>
 
@@ -30,4 +40,31 @@ export default Vue.extend({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
+.search-form {
+  display: flex;
+};
+.nav-input {
+  border: none;
+  border-radius: 0.25rem;
+  padding: 0.5rem;
+  line-height: 1.25;
+  font-size: 1rem;
+  font-family: 'Fira Sans';
+  background-color: #fff;
+}
+.nav-select {
+  border: none;
+  border-radius: 0.25rem;
+  background-color: #c8dbe9;
+  padding: 0.5rem;
+  line-height: 1.25;
+  font-size: 1rem;
+  appearance: none;
+  font-family: 'Fira Sans';
+  padding-right: 1.5rem;
+
+  background-image: url("data:image/svg+xml,%3Csvg width='24' height='16' viewBox='0 0 24 16' xmlns='http://www.w3.org/2000/svg'%3E%3Cpolygon fill='%23000' points='12 1 9 6 15 6' /%3E%3Cpolygon fill='%23000' points='12 13 9 8 15 8' /%3E%3C/svg%3E%0A");
+  background-position: 100% 50%;
+  background-repeat: no-repeat;
+}
 </style>
